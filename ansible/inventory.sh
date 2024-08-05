c4aebc166ec66ea2264fde71571bd57eed75ed9c
9890ABHI <abhipawar7588@gmail.com>
2024-08-05 14:24:55 +0530

    update init

--- ansible/inventory.sh
+++ ansible/inventory.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# read -p "Enter key name :  " key_name 
+
+# read -p "Enter image you to give to instances :  " image_name
+
+# read -p "Enter the subnet id :  " subnet_id
+
+
+# export KEY_NAME=$key_name
+# export IMAGE_NAME=$image_name
+# export SUBNET_NAME=$subnet_id
+
+export KEY_NAME=boot-1
+export IMAGE_NAME=ami-04a81a99f5ec58529
+export SUBNET_NAME=subnet-0af822569e9483069
+
+echo $key_name
+echo $image_name
+echo $subnet_id
+
+
+
 # Run the provisioning playbook and capture output
 ansible-playbook playbooks/provision-ec2.yml | tee provision-output.txt
 
