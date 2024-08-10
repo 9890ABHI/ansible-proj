0f4e70b381a418b6096ca92b716c741f2ff5f93b
9890ABHI <abhipawar7588@gmail.com>
2024-08-10 22:18:35 +0530

    update 1

---
+++ remove.sh
@@ -0,0 +1,13 @@
+
+echo "remove ansible project"
+rm -rf ansible-proj/
+
+echo "#--------------------------------------------------"
+echo "clone the repo"
+git clone https://github.com/9890ABHI/ansible-proj.git
+
+echo "#--------------------------------------------------"
+echo "change the permission for repo"
+chmod -R +x ansible-proj/
+
+echo "#--------------------------------------------------"
